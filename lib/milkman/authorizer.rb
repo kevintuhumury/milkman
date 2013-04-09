43071faa564f9331c4b5b187c150cbdc09a38ad7
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-04-10 00:07:07 +0200

    Split up the authorize method into multiple methods.

--- lib/milkman/authorizer.rb
+++ lib/milkman/authorizer.rb
@@ -15,14 +15,18 @@ module Milkman
 
     def authorize
       puts I18n.t("milkman.authorization.frob_message", url: frob_message_url)
-
-      signed_options = sign options[:shared_secret], options.merge!(method: "rtm.auth.getToken", frob: frob)
-      response       = Milkman::Request.call request_url(signed_options)
-
       puts I18n.t("milkman.authorization.auth_token_message", username: response["rsp"]["auth"]["user"]["username"], api_key: options[:api_key], shared_secret: options[:shared_secret], auth_token: response["rsp"]["auth"]["token"])
     end
 
     private
+
+    def response
+      @response ||= Milkman::Request.call request_url(signed_options)
+    end
+
+    def signed_options
+      sign options[:shared_secret], options.merge!(method: "rtm.auth.getToken", frob: frob)
+    end
 
     def frob
       STDIN.gets.strip
