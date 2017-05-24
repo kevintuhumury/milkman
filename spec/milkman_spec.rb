f8c53632d0332502f9778ec76c2a99512d826c60
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2017-05-24 11:53:34 +0200

    Update the Authorize URL to use https instead of http.

--- spec/milkman_spec.rb
+++ spec/milkman_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Milkman do
 
   it "knows its AUTH_URL" do
-    expect(Milkman::AUTH_URL).to eq "http://www.rememberthemilk.com/services/auth/"
+    expect(Milkman::AUTH_URL).to eq "https://www.rememberthemilk.com/services/auth/"
   end
 
   it "knows its BASE_URL" do
